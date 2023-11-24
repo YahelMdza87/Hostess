53eb018548d6f2d6092b5b8b78aa9a97ecdbdf83
YahelMdza87 <yahelmendoza878@gmail.com>
2023-11-23 19:45:18 -0600

    InsertCategorias

---
+++ ScriptsSQL/insertCategorias.sql
@@ -0,0 +1,9 @@
+INSERT INTO mydb.categorias (Nombre) VALUES ('Entradas');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Desayunos');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Pancakes');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Hamburguesas');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Wraps');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Comidas');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Kids');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Para compartir');
+INSERT INTO mydb.categorias (Nombre) VALUES ('Bebidas');
